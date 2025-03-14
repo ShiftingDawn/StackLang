a174c649ae504955cdfba1464dfddc675e388603
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-03-14 22:53:31 +0100

    Add program parser

--- src/main/java/com/shiftingdawn/stacklang/Main.java
+++ src/main/java/com/shiftingdawn/stacklang/Main.java
@@ -1,12 +1,36 @@
 package com.shiftingdawn.stacklang;
 
+import java.util.Arrays;
+
 public final class Main {
 
 	public static void main(String[] args) {
 		final Stack stack = new Stack();
-		stack.add(1.0);
-		stack.add(2.0);
-		Operator.apply(stack, Op.OP_ADD);
-		Operator.apply(stack, Op.OP_PRINT);
+		String program = "1 2 + .";
+		Object[] parsed = parseProgram(program);
+		System.out.println(Arrays.toString(parsed));
+		for (Object symbol : parsed) {
+			switch (symbol) {
+				case Double d -> stack.push(d);
+				case Op op -> Operator.apply(stack, op);
+				default -> throw new IllegalStateException("Unexpected value: " + symbol);
+			}
+		}
+	}
+
+	public static Object[] parseProgram(String program) {
+		String[] words = program.split("[\\s\\n]+");
+		Object[] result = new Object[words.length];
+		for (int i = 0; i < words.length; ++i) {
+			final String word = words[i];
+			try {
+				double d = Double.parseDouble(word);
+				result[i] = d;
+			} catch (NumberFormatException ignored) {
+				Op op = Op.parse(word).orElseThrow(() -> new IllegalArgumentException("Unknown operation: " + word));
+				result[i] = op;
+			}
+		}
+		return result;
 	}
 }
--- src/main/java/com/shiftingdawn/stacklang/Op.java
+++ src/main/java/com/shiftingdawn/stacklang/Op.java
@@ -1,13 +1,22 @@
 package com.shiftingdawn.stacklang;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum Op {
 
-	OP_ADD(2),
-	OP_PRINT(1);
+	OP_ADD("+", 2),
+	OP_PRINT(".", 1);
 
+	public final String symbol;
 	public final int pops;
 
-	Op(int pops) {
+	Op(String symbol, int pops) {
+		this.symbol = symbol;
 		this.pops = pops;
+	}
+
+	public static Optional<Op> parse(String str) {
+		return Arrays.stream(values()).filter(op -> op.symbol.equals(str)).findFirst();
 	}
 }
